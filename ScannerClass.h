4fa090f7b46e833249c30d79456288c383808cde
Jose Matus <2021154532@ub.edu.bz>
2025-05-05 10:20:35 -0600

    updated class.h

--- ScannerClass.h
+++ ScannerClass.h
@@ -1,16 +1,43 @@
 #ifndef SCANNERCLASS_H
 #define SCANNERCLASS_H
 
+#include <string>
+
 #pragma once
+
+enum TokenType {
+    NUMBER,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE,
+    LPAREN,
+    RPAREN,
+    UNKNOWN,
+    END_OF_INPUT
+};
+
+struct Token {
+    TokenType type;
+    std::string value;
+
+    Token(TokenType typeOfToken, const string& valueOfToken)
+    : type(typeOfToken), value(valueOfToken) {}
+
+};
 
 class ScannerClass
 {
 public:
-    ScannerClass();
-    ~ScannerClass();
+    ScannerClass(const string& input);
+   Token getNextToken();
 
 private:
+string input;
+size_t pos;
 
 };
+
+std::string tokenTypeToString(TokenType type);
 
 #endif
