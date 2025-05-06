7e2af52afb823aaf39d4891dc4a3698fc48b80ef
Jose Matus <2021154532@ub.edu.bz>
2025-05-05 20:01:06 -0600

    updated main

--- Token.cpp
+++ Token.cpp
@@ -1,11 +1,25 @@
-#include <cctype>
 #include <iostream>
 #include <vector>
-using namespace std;
+#include "ScannerClass.h"
 
-int main()
-{
+int main() {
+    std::string input;
+    std::cout << "Enter input: ";
+    std::getline(std::cin, input);
 
+    ScannerClass scanner(input);
+    std::vector<Token> tokens;
 
-    
+    while (true) {
+        Token token = scanner.getNextToken();
+        if (token.type == END_OF_INPUT) break;
+        tokens.push_back(token);
+    }
+
+    for (const Token& token : tokens) {
+        std::cout << "Token(Type: " << tokenTypeToString(token.type)
+                  << ", Value: \"" << token.value << "\")\n";
+    }
+
+    return 0;
 }
