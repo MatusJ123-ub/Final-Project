f0eab6532622bb9d0f6d356d39b402e086498466
Jose Matus <2021154532@ub.edu.bz>
2025-05-10 20:16:27 -0600

    updated main and added some comments to token.h

--- Token.h
+++ Token.h
@@ -4,18 +4,21 @@
 #include <string>
 using namespace std;
 
+// TokenType defines what kind of token will be read from the input 
 enum TokenType {
-    NUMBER,
-    OPERATOR,
-    PARENTHESIS,
-    UNKNOWN,
-    END_OF_INPUT
+    NUMBER,   //Numeric Value
+    OPERATOR, //+,-,*,/
+    PARENTHESIS,// open and close parenthesis
+    UNKNOWN,  //Anything that doesn't fit in the other categories
+    END_OF_INPUT //Token that indicates we are done scanning
 };
 
+//Token hold both type and the value of the token
 struct Token {
     TokenType type;
     string value;
 
+    //Constructor 
     Token(TokenType typeOfToken, const string& valueOfToken);
 };
 
