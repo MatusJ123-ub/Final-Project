35b67840a6cd5ca241a93787d8f84ef0c4bdec6d
Jose Matus <2021154532@ub.edu.bz>
2025-05-10 20:03:55 -0600

    updated ScannerClass.cpp

--- ScannerClass.cpp
+++ ScannerClass.cpp
@@ -34,30 +34,14 @@ Token getNextToken(const std::string& input, size_t& pos) {
         return Token(NUMBER, number);
     }
 
-    switch (curr) {
-        case '+': pos++; return Token(PLUS, "+");
-        case '-': pos++; return Token(MINUS, "-");
-        case '*': pos++; return Token(MULTIPLY, "*");
-        case '/': pos++; return Token(DIVIDE, "/");
-        case '(': pos++; return Token(LPAREN, "(");
-        case ')': pos++; return Token(RPAREN, ")");
-        default:
-            std::string unknown;
-            unknown += input[pos++];
-            return Token(UNKNOWN, unknown);
-    }
-}
+    if (curr == '+' || curr == '-' || curr == '*' || curr == '/' || curr == '%'){
+        pos++;
+        return Token(OPERATOR, std::string(1,curr));
 
-std::string tokenTypeToString(TokenType type) {
-    switch (type) {
-        case NUMBER: return "NUMBER";
-        case PLUS: return "PLUS";
-        case MINUS: return "MINUS";
-        case MULTIPLY: return "MULTIPLY";
-        case DIVIDE: return "DIVIDE";
-        case LPAREN: return "LPAREN";
-        case RPAREN: return "RPAREN";
-        case UNKNOWN: return "UNKNOWN";
-        default: return "END_OF_INPUT";
     }
+
+    std::string unknown;
+    unknown += input[pos++];
+    return Token(UNKNOWN, unknown);
+
 }
